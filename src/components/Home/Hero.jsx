935ec3963aa208ff590dacdaa31ba1e8764c19e0
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-07 09:01:13 +0100

    Created hero component

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,10 @@
+import Image from 'mui-image';
+import hero from '../../assets/hero.png';
+
+export default function Hero() {
+  return (
+    <>
+      <Image src={hero} height='fit-content' maxHeight={500} fit='contain' />
+    </>
+  );
+}
