9c828e224f52058f0988aed9408f7b7437f040a3
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-07 09:02:56 +0100

    Created search component for handling and rendering search

---
+++ src/components/Home/Search.jsx
@@ -0,0 +1,148 @@
+import {
+  IconButton,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  TextField,
+  Typography,
+  styled,
+  Box,
+  CardHeader,
+  Divider,
+} from '@mui/material';
+import { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+const SearchContainer = styled(Container)`
+  position: absolute;
+  background-color: white;
+  z-index: 5;
+  margin: 0;
+  padding: 1.5em;
+  box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.2);
+`;
+
+const GridBox = styled(Box)`
+  display: grid;
+  padding: 0.2em;
+  padding-left: 0;
+  padding-right: 0;
+  gap: 1em;
+  border-radius: 0.2em;
+  margin-top: 0.2em;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media only screen and (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media only screen and (max-width: 700px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export function Search({ data }) {
+  const [searchField, setSearchField] = useState('');
+
+  const filteredItems = data.filter((item) => {
+    if (
+      item.title.toLowerCase().startsWith(searchField.toLowerCase()) &&
+      searchField.length > 0
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+  const resetField = () => {
+    document.querySelector('#searchField').value = '';
+    setSearchField('');
+    display = '';
+  };
+
+  let display;
+  filteredItems.length === 0 && searchField.length > 0
+    ? (display = (
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{ textAlign: 'center', marginTop: 1 }}
+        >
+          No items found.
+        </Typography>
+      ))
+    : (display = (
+        <GridBox>
+          {filteredItems &&
+            filteredItems.map((product) => (
+              <Card key={product.id}>
+                <CardMedia
+                  sx={{ height: 120, objectFit: 'cover' }}
+                  image={product.imageUrl}
+                  title={product.title}
+                />
+
+                <CardContent>
+                  <Typography
+                    component='h2'
+                    variant='h6'
+                    sx={{ padding: 0, margin: 0, paddingBottom: 0 }}
+                  >
+                    {product.title}
+                  </Typography>
+                  <Typography
+                    component='p'
+                    variant='body2'
+                    sx={{ padding: 0, margin: 0, paddingBottom: 0 }}
+                  >
+                    {product.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
+        </GridBox>
+      ));
+
+  return (
+    <>
+      <Box sx={{ position: 'relative', marginTop: 2, width: '100%' }}>
+        <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+          <TextField
+            sx={{ margin: 0 }}
+            onChange={handleChange}
+            label='Search All Products'
+            fullWidth
+            variant='standard'
+            id='searchField'
+          />
+          {searchField && (
+            <IconButton
+              sx={{ height: 'min-content' }}
+              size='small'
+              variant='contained'
+              onClick={resetField}
+            >
+              <CloseIcon />
+            </IconButton>
+          )}
+        </Box>
+        {searchField && (
+          <SearchContainer>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              {' '}
+              <Typography>Search results for: {searchField}</Typography>
+              <Typography>Total: {filteredItems.length}</Typography>
+            </Box>
+            <Divider />
+            {display}
+          </SearchContainer>
+        )}
+      </Box>
+    </>
+  );
+}
