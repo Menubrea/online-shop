44c99b1248ac51cfa314181e5a911db0400231c5
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-14 09:19:31 +0100

    Created layout component

--- src/components/UI/Layout.jsx
+++ src/components/UI/Layout.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { ShoppingCart } from './ShoppingCart';
 
 export function Layout({ state, dispatch }) {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <>
       <Header />
-      <ShoppingCart state={state} dispatch={dispatch} />
+      {pathname !== '/checkout' && <ShoppingCart state={state} dispatch={dispatch} />}
       <Outlet />
       <Footer />
     </>
