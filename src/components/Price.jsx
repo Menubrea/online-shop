452e7ee86836ffb57cc7c1c14a5fc330f4f263c5
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-14 09:27:43 +0100

    Created price component

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,54 +1,52 @@
-import { Box, Typography } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 
-export default function Price({ product }) {
-  let sum;
-  sum = product.price - product.discountedPrice;
-  let stringedSum = sum.toString();
+const StyledBox = styled(Box)`
+  backdrop-filter: blur(10px);
+  background-color: #ffffff87;
+  padding: 0 0.5em;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export default function Price({ product, position }) {
+  // Calculate and stringify the difference to allow the use of string methods.
+  let difference;
+  difference = product.price - product.discountedPrice;
+  let stringedDifference = difference.toString();
 
   return (
-    <Box
+    <StyledBox
       sx={{
-        position: 'absolute',
-        display: 'flex',
-        backgroundColor: 'primary.dark',
-        color: 'white.light',
-        paddingX: 1,
+        position: { position },
         bottom: 0,
-        right: 0,
-        textAlign: 'center',
-        minHeight: 65,
-        minWidth: 130,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
+      }}>
       {/* Display this if discounted and price does not match */}
       {product.discountedPrice !== product.price ? (
+        <>
+          <Box>
+            <Typography variant='body2' component='s'>
+              {product.price},-
+            </Typography>{' '}
+            <Typography sx={{ fontWeight: 700 }} variant='body2' component='strong'>
+              {product.discountedPrice},-
+            </Typography>{' '}
+          </Box>
+          <Typography sx={{ fontWeight: 700 }} variant='body1' component='p'>
+            You save {stringedDifference.split('.')[0]},-
+          </Typography>{' '}
+        </>
+      ) : (
         <Box>
-          <Typography variant='body2' component='s'>
+          <Typography variant='body1' component='div'>
+            {/* Else This */}
             {product.price} kr
-          </Typography>{' '}
-          <Typography
-            sx={{ fontWeight: 700 }}
-            variant='body2'
-            component='strong'
-          >
-            {product.discountedPrice} kr
-          </Typography>{' '}
-          <Typography
-            sx={{ padding: 0, margin: 0 }}
-            variant='body1'
-            component='div'
-          >
-            [You save {stringedSum.split('.')[0]} kr]
           </Typography>
         </Box>
-      ) : (
-        <Typography variant='body1' component='div'>
-          {/* Else This */}
-          {product.price} kr
-        </Typography>
       )}
-    </Box>
+    </StyledBox>
   );
 }
