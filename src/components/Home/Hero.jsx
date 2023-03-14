4df28b2611622f63198782ae38e3c625e4d28d55
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-14 17:23:04 +0100

    Made changes to hero component for handling visibility

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,10 +1,48 @@
 import Image from 'mui-image';
 import hero from '../../assets/hero.png';
+import { useState, useEffect } from 'react';
+import { Button, Box, styled } from '@mui/material';
+
+const StyledBox = styled(Box)`
+  animation: Fade 1s ease-in;
+
+  @keyframes Fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
 
 export default function Hero() {
+  const [isShown, setIsShown] = useState(true);
+
+  function DisplayHero(boolean) {
+    setIsShown(boolean);
+    sessionStorage.setItem('isShown', String(boolean));
+  }
+
+  useEffect(() => {
+    const isSaved = sessionStorage.getItem('isShown');
+    isSaved && setIsShown(JSON.parse(isSaved));
+  }, [isShown]);
+
   return (
     <>
-      <Image src={hero} height='fit-content' maxHeight={500} fit='contain' title='page-hero' duration={1000} />
+      <Box>
+        {isShown && (
+          <StyledBox>
+            <Image src={hero} height='fit-content' maxHeight={500} fit='contain' title='page-hero' duration={1000} />
+          </StyledBox>
+        )}
+        {isShown && (
+          <Button variant='contained' fullWidth onClick={() => DisplayHero(false)}>
+            Hide hero
+          </Button>
+        )}
+      </Box>
     </>
   );
 }
