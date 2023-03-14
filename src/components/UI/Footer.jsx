05a6622dcc90a0e9595d40983e40fd861a3b0162
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-14 09:19:00 +0100

    Created footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -2,8 +2,12 @@ import { Box, Typography } from '@mui/material';
 
 export function Footer() {
   return (
-    <Box component='footer' sx={{ backgroundColor: 'primary.main' }}>
-      <Typography variant='body2' sx={{ textAlign: 'center' }}>
+    <Box
+      component='footer'
+      sx={{ backgroundColor: 'primary.main' }}>
+      <Typography
+        variant='body2'
+        sx={{ textAlign: 'center' }}>
         All Rights Reserved | Menubrea
       </Typography>
     </Box>
