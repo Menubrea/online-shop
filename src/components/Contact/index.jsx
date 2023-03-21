3befa36b0577d2a66a7ec2b8642804fe1e425b8d
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-21 18:03:11 +0100

    Added components to main contact component

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Typography, Container } from '@mui/material';
+import Image from 'mui-image';
+import hero from '../../assets/hero.png';
+import { ContactForm } from './ContactForm';
+import { Faq } from './Faq';
+
+export function Contact() {
+  return (
+    <Box sx={{ display: 'grid', gridTemplateColumns: { sm: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }, maxWidth: 1920 }} component='main'>
+      <Box sx={{ backgroundColor: 'black.light' }}>
+        <Image height={300} src={hero} title='hero' fit='contain' />
+        <Faq />
+      </Box>
+      <Box sx={{ backgroundColor: 'black.light' }}>
+        <Typography sx={{ backgroundColor: 'black.light', padding: '.5em 1em', color: 'white.main' }} variant='h4' component='h1'>
+          Contact Us
+        </Typography>
+        <ContactForm />
+      </Box>
+    </Box>
+  );
+}
