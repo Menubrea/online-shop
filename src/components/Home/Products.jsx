b6c1674b75b8a7d24b6f3d25c9a50a8e600b4b33
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-21 18:02:22 +0100

    Adjusted layout of products

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -4,13 +4,17 @@ import { styled } from '@mui/material/styles';
 
 const MainGrid = styled(Container)`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 1em;
   max-width: 100%;
   margin: 0 auto 2em;
   min-height: 50vh;
 
-  @media only screen and (max-width: 1000px) {
+  @media only screen and (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media only screen and (max-width: 870px) {
     grid-template-columns: repeat(2, 1fr);
   }
 
