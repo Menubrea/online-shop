5711bc9e20766f3a1d30103a7452ea69d9a2b4c2
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-28 10:06:48 +0200

    Removed Popup() from layout

--- src/components/UI/Layout.jsx
+++ src/components/UI/Layout.jsx
@@ -1,17 +1,14 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import { Popup } from '../Utilities/Popup';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { ShoppingCart } from './ShoppingCart';
 
 export function Layout({ state, dispatch }) {
   const { pathname } = useLocation();
-  console.log(pathname);
   return (
     <>
       <Header />
       {pathname !== '/checkout' && <ShoppingCart state={state} dispatch={dispatch} />}
-      {pathname !== '/checkout' && <Popup state={state} />}
       <Outlet />
       <Footer />
     </>
