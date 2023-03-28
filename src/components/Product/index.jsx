70ddf45c5302ef771ce1b5d3c3b2c12fdec0a92c
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-28 20:05:20 +0200

    Removed guard clause to see if thats whats causing the deploy bug.

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -59,64 +59,58 @@ export default function ProductPage({ data, state, dispatch }) {
     });
   }, [id, data, filterProduct]);
 
-  if (data) {
-    return (
-      <Box
+  return (
+    <Box
+      sx={{
+        marginY: 2,
+      }}
+      component='main'>
+      <MetaData title={`Re:mote | ${filterProduct.title}`} description={`Re:mote | ${filterProduct.description}`} tags={filterProduct.tags.map((tag) => tag)} />
+      <BreadCrumbs product={filterProduct} />
+      <Container
         sx={{
-          marginY: 2,
-        }}
-        component='main'>
-        <MetaData
-          title={`Re:mote | ${filterProduct.title}`}
-          description={`Re:mote | ${filterProduct.description}`}
-          tags={filterProduct.tags.map((tag) => tag)}
-        />
-        <BreadCrumbs product={filterProduct} />
-        <Container
-          sx={{
-            display: 'grid',
-            gap: 2,
-            gridTemplateColumns: {
-              sm: 'repeat(1, 1fr)',
-              md: 'repeat(3, 1fr)',
-            },
-          }}>
-          <Box sx={{ gridColumn: { md: '1 / 3', xs: '1 / 4' }, height: 'max-content', position: 'relative', overflow: 'hidden' }}>
-            <ProductDetails data={filterProduct} />
-            {filterProduct.discountedPrice !== filterProduct.price && <Sale product={filterProduct} />}
-          </Box>
-          <Box
-            sx={{
-              gridColumn: { md: '3 / 4', xs: '1 / 4' },
-              padding: 2,
-              border: 0.5,
-              borderStyle: 'solid',
-              borderRadius: 2,
-              borderColor: 'rgba(0, 0, 0, .2)',
-            }}>
-            <AddToCart data={filterProduct} dispatch={dispatch} state={state} />
-            <Reviews data={filterProduct} />
-            <StoreInformation />
-          </Box>
-        </Container>
-        <Divider sx={{ marginY: 2 }} />
-        <Box>
-          <Container>
-            <Typography variant='h6' component='h2'>
-              You might also like:
-            </Typography>
-          </Container>
-          {filteredProducts.length > 0 ? (
-            <Products data={filteredProducts} state={state} headerElement='h3' />
-          ) : (
-            <Box sx={{ margin: '0 auto', width: 'fit-content', padding: '.2em .5em', borderRadius: 2, backgroundColor: 'black.light', color: 'white.light' }}>
-              {' '}
-              No matches found{' '}
-            </Box>
-          )}
-          <StyledLink to='/'>Return Home</StyledLink>
+          display: 'grid',
+          gap: 2,
+          gridTemplateColumns: {
+            sm: 'repeat(1, 1fr)',
+            md: 'repeat(3, 1fr)',
+          },
+        }}>
+        <Box sx={{ gridColumn: { md: '1 / 3', xs: '1 / 4' }, height: 'max-content', position: 'relative', overflow: 'hidden' }}>
+          <ProductDetails data={filterProduct} />
+          {filterProduct.discountedPrice !== filterProduct.price && <Sale product={filterProduct} />}
         </Box>
+        <Box
+          sx={{
+            gridColumn: { md: '3 / 4', xs: '1 / 4' },
+            padding: 2,
+            border: 0.5,
+            borderStyle: 'solid',
+            borderRadius: 2,
+            borderColor: 'rgba(0, 0, 0, .2)',
+          }}>
+          <AddToCart data={filterProduct} dispatch={dispatch} state={state} />
+          <Reviews data={filterProduct} />
+          <StoreInformation />
+        </Box>
+      </Container>
+      <Divider sx={{ marginY: 2 }} />
+      <Box>
+        <Container>
+          <Typography variant='h6' component='h2'>
+            You might also like:
+          </Typography>
+        </Container>
+        {filteredProducts.length > 0 ? (
+          <Products data={filteredProducts} state={state} headerElement='h3' />
+        ) : (
+          <Box sx={{ margin: '0 auto', width: 'fit-content', padding: '.2em .5em', borderRadius: 2, backgroundColor: 'black.light', color: 'white.light' }}>
+            {' '}
+            No matches found{' '}
+          </Box>
+        )}
+        <StyledLink to='/'>Return Home</StyledLink>
       </Box>
-    );
-  }
+    </Box>
+  );
 }
