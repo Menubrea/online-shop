9a57c6ec8b7bdd19218114901407bce0b85cf421
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-03-28 10:08:09 +0200

    Wrapped component in guard clause

--- src/components/Product/ProductDetails.jsx
+++ src/components/Product/ProductDetails.jsx
@@ -4,32 +4,34 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 export function ProductDetails({ data }) {
-  return (
-    <>
-      <Image src={data.imageUrl} title={data.title} sx={{ maxHeight: 500, borderRadius: 1 }} />
-      <Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: 2 }}>
-          <Typography variant='h4' component='h1' sx={{ fontFamily: 'arbotek', fontWeight: 900, color: 'black.light' }}>
-            {data.title}
-          </Typography>
-          {data.rating > 0 ? (
-            <Rating
-              sx={{ color: 'primary.main', marginRight: 3 }}
-              name='rating'
-              value={data.rating}
-              precision={0.5}
-              readOnly
-              icon={<FavoriteIcon fontSize='inherit' />}
-              emptyIcon={<FavoriteBorderIcon sx={{ color: 'primary.main' }} fontSize='inherit' />}
-            />
-          ) : (
-            <Typography>No Rating</Typography>
-          )}
+  if (data) {
+    return (
+      <>
+        {data && <Image src={data.imageUrl} title={data.title} sx={{ maxHeight: 500, borderRadius: 1 }} />}
+        <Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: 2 }}>
+            <Typography variant='h4' component='h1' sx={{ fontFamily: 'arbotek', fontWeight: 900, color: 'black.light' }}>
+              {data.title}
+            </Typography>
+            {data.rating > 0 ? (
+              <Rating
+                sx={{ color: 'primary.main', marginRight: 3 }}
+                name='rating'
+                value={data.rating}
+                precision={0.5}
+                readOnly
+                icon={<FavoriteIcon fontSize='inherit' />}
+                emptyIcon={<FavoriteBorderIcon sx={{ color: 'primary.main' }} fontSize='inherit' />}
+              />
+            ) : (
+              <Typography>No Rating</Typography>
+            )}
+          </Box>
+          <CardContent sx={{ marginY: 0, paddingY: 0 }}>
+            <Typography>{data.description}</Typography>
+          </CardContent>
         </Box>
-        <CardContent sx={{ marginY: 0, paddingY: 0 }}>
-          <Typography>{data.description}</Typography>
-        </CardContent>
-      </Box>
-    </>
-  );
+      </>
+    );
+  }
 }
