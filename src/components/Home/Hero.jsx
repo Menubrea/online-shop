4129a750b1816922db62a3c384994c8c9bd5514c
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-04-02 13:36:15 +0200

    Removed animation from hero

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,20 +1,6 @@
-import Image from 'mui-image';
 import hero from '../../assets/hero.png';
 // import { useState, useEffect } from 'react';
 import { Box, styled, Container } from '@mui/material';
-
-const StyledBox = styled(Box)`
-  animation: Fade 1s ease-in;
-
-  @keyframes Fade {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-`;
 
 /**
  * Component for rendering Hero
@@ -23,9 +9,9 @@ const StyledBox = styled(Box)`
 export default function Hero() {
   return (
     <Container sx={{ position: 'relative', display: { xs: 'none', sm: 'block' } }}>
-      <StyledBox sx={{ marginTop: 2 }}>
-        <Image src={hero} height='fit-content' maxHeight={500} fit='contain' title='page-hero' duration={100} sx={{ borderRadius: 5 }} />
-      </StyledBox>
+      <Box sx={{ marginTop: 2 }}>
+        <Box component='img' src={hero} alt={'hero'} height='fit-content' maxHeight={500} fit='contain' title='page-hero' sx={{ borderRadius: 5 }} />
+      </Box>
     </Container>
   );
 }
